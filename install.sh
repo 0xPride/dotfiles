dea2f7ad21ba2632eaff6c66f97dfa57df9d9b00
project-HOSSAM <32068899+project-HOSSAM@users.noreply.github.com>
2021-10-06 05:05:41 +0100

    writing new install script and moving to gnu stow

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "
+╱╱╱╱╱╱╱╱╭╮╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮
+╱╱╱╱╱╱╱╱┃┃╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╭╮╭┳━━┳━╯┣━━╮┃╰━┳╮╱╭╮╭━━┳━┳┳━╯┣━━╮
+┃╰╯┃╭╮┃╭╮┃┃━┫┃╭╮┃┃╱┃┃┃╭╮┃╭╋┫╭╮┃┃━┫
+┃┃┃┃╭╮┃╰╯┃┃━┫┃╰╯┃╰━╯┃┃╰╯┃┃┃┃╰╯┃┃━┫
+╰┻┻┻╯╰┻━━┻━━╯╰━━┻━╮╭╯┃╭━┻╯╰┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯╱╰╯"
+
+printf "\n\n"
+
+printf "updating"
+sudo pacman -Syu
+sudo pacman -Syy
+
+printf "Installing zsh"
+yay -S zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+printf "\n\n"
+
+printf "Installing YAY"
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay; makepkg-si; cd
+printf "\n\n"
+
+printf "Installing Python"
+yay -S python
+printf "\n\n"
+
+printf "Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash 
+printf "\n\n"
+
+printf "Installing Neovim"
+yay -S neovim
+printf "\n\n"
+
+printf "Installing LunarVim"
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+printf "\n\n"
+
+printf "Installing Alacritty"
+yay -S alacritty
+printf "\n\n"
+
+printf "Installing Kitty"
+yay -S kitty
+printf "\n\n"
+
+printf "Installing AwesomeWM"
+yay -S awesome
+printf "\n\n"
+
+printf "Installing Gnu Stow"
+yay -S stow
+printf "\n\n"
+
+printf "Installing Wine And Lutris"
+yay -S wine lutris
+printf "\n\n"
+
+stow */
