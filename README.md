b3e69cd918f92fab1c75fc29e515cac7c01a7e66
project-HOSSAM <32068899+project-HOSSAM@users.noreply.github.com>
2021-10-06 08:29:40 +0100

    adding neovide and readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Dotefiles
+
+my dotefiles with install script that installs all my preferred programs.
+
+## Installation
+
+Use git to clone the repo.
+
+```bash
+git clone https://github.com/project-HOSSAM/Dotefiles.git ~/Dotefiles
+```
+
+## Usage
+
+```shell
+cd ~/Dotefiles;
+chmod +x install.sh
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -42,6 +42,17 @@ printf "Installing LunarVim"
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 printf "\n\n"
 
+printf "Installing Neovide"
+yay -S neovide
+echo '#!/bin/sh
+
+export LUNARVIM_CONFIG_DIR="${LUNARVIM_CONFIG_DIR:-$HOME/.config/lvim}"
+export LUNARVIM_RUNTIME_DIR="${LUNARVIM_RUNTIME_DIR:-$HOME/.local/share/lunarvim}"
+export LUNARVIM_CACHE_DIR="${LUNARVIM_CACHE_DIR:-$HOME/.cache/lvim}"
+
+exec neovide -- -u "$LUNARVIM_RUNTIME_DIR/lvim/init.lua" "$@"' > ~/.local/bin/lvim-gui
+chmod +x ~/.local/bin/lvim-gui
+
 printf "Installing Alacritty"
 yay -S alacritty
 printf "\n\n"
