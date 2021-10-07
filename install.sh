67b92965998c36282236690034498548e2223497
project-HOSSAM <32068899+project-HOSSAM@users.noreply.github.com>
2021-10-07 04:06:07 +0100

    fixing installer bugs

--- install.sh
+++ install.sh
@@ -12,38 +12,29 @@ echo "
 
 printf "\n\n"
 
-printf "updating"
 sudo pacman -Syu
 sudo pacman -Syy
 
-printf "Installing zsh"
-yay -S zsh
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg -si
+cd ~/Dotefiles
+
+yay -S zsh python neovim neovide alacritty kitty awesome stow feh
+
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 printf "\n\n"
 
-printf "Installing YAY"
-git clone https://aur.archlinux.org/yay.git /tmp/yay
-cd /tmp/yay; makepkg-si; cd
-printf "\n\n"
-
-printf "Installing Python"
-yay -S python
-printf "\n\n"
-
-printf "Installing NVM"
+# node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash 
 printf "\n\n"
 
-printf "Installing Neovim"
-yay -S neovim
-printf "\n\n"
-
-printf "Installing LunarVim"
+# lunar vim
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 printf "\n\n"
 
-printf "Installing Neovide"
-yay -S neovide
+# neovide luncher 
 echo '#!/bin/sh
 
 export LUNARVIM_CONFIG_DIR="${LUNARVIM_CONFIG_DIR:-$HOME/.config/lvim}"
@@ -53,29 +44,6 @@ export LUNARVIM_CACHE_DIR="${LUNARVIM_CACHE_DIR:-$HOME/.cache/lvim}"
 exec neovide -- -u "$LUNARVIM_RUNTIME_DIR/lvim/init.lua" "$@"' > ~/.local/bin/lvim-gui
 chmod +x ~/.local/bin/lvim-gui
 
-printf "Installing Alacritty"
-yay -S alacritty
-printf "\n\n"
-
-printf "Installing Kitty"
-yay -S kitty
-printf "\n\n"
-
-printf "Installing AwesomeWM"
-yay -S awesome
-printf "\n\n"
-
-printf "Installing Gnu Stow"
-yay -S stow
-printf "\n\n"
-
-printf "Installing Wine And Lutris"
-yay -S wine lutris
-printf "\n\n"
-
-printf "Installing Wine And Lutris"
-yay -S feh
-printf "\n\n"
 
 stow */
 feh --bg-scale ~/Pictures/Wallpapers/1109233.jpg
