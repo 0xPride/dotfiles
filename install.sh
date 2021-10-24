9bf696594c8000136cada426f092a808823f8c79
project-HOSSAM <32068899+project-HOSSAM@users.noreply.github.com>
2021-10-24 22:03:02 +0000

    removing xorg

--- install.sh
+++ install.sh
@@ -18,10 +18,10 @@ cd /tmp/yay
 makepkg -si
 cd ~/Dotefiles
 
-yay -S zsh firefox python neovim neovide alacritty kitty stow feh picom rofi zathura zathura-pdf-poppler man-db man-pager bat scrot htop neofetch ffmpeg xorg-xrdb mpv-full qbittorrent light-git paulseaudio alsa-lib alsa-plugins alsa-utils alsa-firmware sway swayidle swaylock waybar swappy grim wofi nerd-fonts-mononoki gammastep slurp imv mako
+yay -S firefox python neovim alacritty kitty stow zathura zathura-pdf-poppler man-db man-pager bat htop neofetch ffmpeg xorg-xwayland mpv-full light-git paulseaudio alsa-lib alsa-plugins alsa-utils alsa-firmware sway swayidle swaylock waybar swappy grim wofi  gammastep slurp imv mako
 
 # fonts
-yay -S ttf-hack ttf-font-awesome ttf-mononoki
+yay -S ttf-hack ttf-font-awesome ttf-mononoki nerd-fonts-mononoki
 # node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash 
 
@@ -40,7 +40,6 @@ chmod +x ~/.local/bin/lvim-gui
 
 stow ~/Dotefiles/*/
 
-# oh-my-zsh
+# zsh
+yay -S zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# TODO: setup dunst
